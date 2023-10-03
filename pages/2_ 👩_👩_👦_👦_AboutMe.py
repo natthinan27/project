3d3daee8b26a09c9701de8059f338b5335ab5da2
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-10-03 15:13:30 +0700

    .commit

--- pages/2_ 👩_👩_👦_👦_AboutMe.py
+++ pages/2_ 👩_👩_👦_👦_AboutMe.py
@@ -11,7 +11,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
+lottie_url_hello = "https://lottie.host/ed7417b2-8e41-4fd9-bad4-426bb7b82d1f/lywBmSfE6B.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello, key="hello")
 
