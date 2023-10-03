e2b5e66594eca5b648c3a31e068d5752eeb3ee58
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-10-03 15:45:20 +0700

    .commit

--- pages/4_New.py
+++ pages/4_New.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 
 st.header("Show Data Index Price")
-df=pd.read_csv("./C:\AppServ\MySQL\project1\project\stock_index_price-2.csv")
+df=pd.read_csv("./Data/stock_index_price.csv")
 st.write(df.head(10))
 
 
