8d21243b5b670980cd3228871a3b8fe36292e707
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-10-03 15:55:33 +0700

    Update 4_New.py

--- pages/4_New.py
+++ pages/4_New.py
@@ -8,4 +8,8 @@ st.header("Show Data Index Price")
 df=pd.read_csv("./Data/stock_index_price.csv")
 st.write(df.head(10))
 
+st.line_chart(
+   chart_data, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+)
+
 
