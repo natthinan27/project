7c137020ab0fde64dbeeedc859f55dfc61683adf
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-10-03 15:39:49 +0700

    .commit

--- pages/4_New.py
+++ pages/4_New.py
@@ -5,13 +5,8 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+st.header("Show Data Index Price")
+df=pd.read_csv("./C:\AppServ\MySQL\project1\project\stock_index_price-2.csv")
+st.write(df.head(10))
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-st_lottie(lottie_hello, key="hello")
 
