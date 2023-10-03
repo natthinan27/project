0a82cf333e9020c2080e70c920225a2c1ea580cb
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-10-03 15:41:54 +0700

    .commit

--- pages/4_New.py
+++ pages/4_New.py
@@ -2,8 +2,7 @@ import json
 import time
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
+import pandas as pd
 
 st.header("Show Data Index Price")
 df=pd.read_csv("./C:\AppServ\MySQL\project1\project\stock_index_price-2.csv")
