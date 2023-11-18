9eed89a200b9f0713a1a4d63338ce8ed8dfdfa58
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-18 17:18:19 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -10,8 +10,9 @@ st.write(df.head(10))
 
 st.header("Show chart")
 chart_data=pd.read_csv("./Data/shopping22.csv")
-st.line_chart(
-   chart_data, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+plot(df)
+#st.line_chart(
+ #  chart_data, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
 )
 
 
