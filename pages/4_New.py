99248160f6fee3acf526276212faf668fcea7be7
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-18 17:09:14 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -5,11 +5,11 @@ import streamlit as st
 import pandas as pd
 
 st.header("Show Data Index Price")
-df=pd.read_csv("./Data/stock_index_price.csv")
+df=pd.read_csv("./Data/shopping22.csv")
 st.write(df.head(10))
 
 st.header("Show chart")
-chart_data=pd.read_csv("./Data/stock_index_price.csv")
+chart_data=pd.read_csv("./Data/shopping22.csv")
 st.line_chart(
    chart_data, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
 )
