cb727fc9fce41fa1daacbe98e52e0bed138376da
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-18 17:31:21 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -11,22 +11,12 @@ st.write(df.head(10))
 st.header("Show chart")
 chart_data=pd.read_csv("./Data/shopping22.csv")
 
-import matplotlib.pyplot as plt
+dftext=pd.DataFrame(df)
+st.bar_chart(dftext['Item Purchased'].value_counts())
 
-def plot(df):
-    # Select a column to plot
-    column_name = 'Age'
 
-    # Create a line plot of the selected column
-    plt.plot(df['index'], df[column_name])
 
-    # Add labels and title to the plot
-    plt.xlabel('index')
-    plt.ylabel(column_name)
-    plt.title('Plot of ' + column_name)
 
-    # Display the plot
-    plt.show()
 #st.line_chart(
  #  chart_data, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional)
 
