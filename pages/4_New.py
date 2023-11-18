2514f9a8b68048fa67864313bfed40455461d427
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-18 17:22:57 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -10,6 +10,11 @@ st.write(df.head(10))
 
 st.header("Show chart")
 chart_data=pd.read_csv("./Data/shopping22.csv")
+
+# Load your data into a DataFrame
+df = pd.read_csv('data.csv')
+
+# Call the plot function to generate a plot of the data
 plot(df)
 #st.line_chart(
  #  chart_data, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional)
