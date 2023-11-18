a4541ad9f7aa38b8d238dc63e35a547ce72d3ddb
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-18 17:42:05 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -3,6 +3,8 @@ import time
 import requests
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 st.header("Show Data Index Price")
 df=pd.read_csv("./Data/shopping22.csv")
@@ -11,9 +13,15 @@ st.write(df.head(10))
 st.header("Show chart")
 chart_data=pd.read_csv("./Data/shopping22.csv")
 
-dftext=pd.DataFrame(df)
-st.bar_chart(dftext['Item Purchased','Color'].value_counts())
-
+#dftext=pd.DataFrame(df)
+#st.bar_chart(dftext['Item Purchased','Color'].value_counts())
+grouped_data = df.groupby(['Age', 'Color']).size().reset_index(name='count')
+pivot_table = pd.pivot_table(grouped_data, values='count', index='Age', columns='Color', fill_value=0)
+fig, ax = plt.subplots(figsize=(15, 7))
+colors = ['#57b199', '#7fc15a', '#ffa53b']
+pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
+plt.axis('equal')
+st.pyplot(fig)
 
 
 
