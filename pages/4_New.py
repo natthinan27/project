bf3c3888e99f6d8e97473bda7a04e8485646c0c9
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-19 20:18:50 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -20,8 +20,8 @@ chart_data=pd.read_csv("./Data/shopping22.csv")
 grouped_data = df.groupby(['Season', 'Gender']).size().reset_index(name='count')
 pivot_table = pd.pivot_table(grouped_data, values='count', index='Season', columns='Gender', fill_value=0)
 fig, ax = plt.subplots(figsize=(15, 7))
-color = ['#F7DC6F', '#7fc15a', '#ffa53b','#BB8FCE']
-pivot_table.plot.bar(subplots=True, autopct='%1.1f%%', ax=ax, color=color)
+colors = ['#F7DC6F', '#7fc15a', '#ffa53b','#BB8FCE']
+pivot_table.plot.bar(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
 plt.axis('equal')
 st.pyplot(fig)
 
