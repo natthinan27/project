d98eafa54d5bdb8c3672e126a25e5404225009f8
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 00:37:57 +0700

    .c

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -15,8 +15,4 @@ if fl is not None:
     filename = fl.name
     st.write(filename)
     df = pd.read_csv(filename,encoding ="ISO-8859-1")
-else:
-   os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    df = pd.read_csv("Superstore.csv",encoding ="ISO-8859-1")
-
-                                                                                                                                    
+                                                                    
