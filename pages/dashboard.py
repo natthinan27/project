e89119ffa1352adf16913ae06677938fb70932cd
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 01:09:41 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -21,6 +21,6 @@ else:
     #file_path = r"C:\Users\acer\Desktop\shopping\your_file.txt"         
     #file_path = "your_file.txt"
     #file_path = "subfolder/your_file.txt"
-file_path = "/absolute/path/to/shopping22.csv"
-file_path = "shopping22.csv"
-df = pd.read_csv(file_path, encoding="ISO-8859-1")
+file_path ="/absolute/path/to/shopping22.csv"
+file_path ="shopping22.csv"
+df = pd.read_csv(file_path,encoding="ISO-8859-1")
