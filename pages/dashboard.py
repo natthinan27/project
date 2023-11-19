7f0fdc5ec3c2fa35be1d2b59e87d92806f6ba8c8
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 00:40:40 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -15,4 +15,7 @@ if fl is not None:
     filename = fl.name
     st.write(filename)
     df = pd.read_csv(filename,encoding ="ISO-8859-1")
+else:
+    os.chdir(r"C:\Users\AEPAC\Desktop\Streamlit")
+    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
                                                                     
