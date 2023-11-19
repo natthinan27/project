4361828221cdf7683ba989c1b7245e4a1e76f1cb
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-19 20:11:10 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -17,24 +17,24 @@ chart_data=pd.read_csv("./Data/shopping22.csv")
 #dftext=pd.DataFrame(df)
 #st.bar_chart(dftext['Item Purchased','Color'].value_counts())
 
-#grouped_data = df.groupby(['Season', 'Gender']).size().reset_index(name='count')
-#pivot_table = pd.pivot_table(grouped_data, values='count', index='Season', columns='Gender', fill_value=0)
-#fig, ax = plt.subplots(figsize=(15, 7))
-#colors = ['#57b199', '#7fc15a', '#ffa53b','#BB8FCE']
-#pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
-#plt.axis('equal')
-#st.pyplot(fig)
-
+grouped_data = df.groupby(['Season', 'Gender']).size().reset_index(name='count')
+pivot_table = pd.pivot_table(grouped_data, values='count', index='Season', columns='Gender', fill_value=0)
 fig, ax = plt.subplots(figsize=(15, 7))
 colors = ['#F7DC6F', '#7fc15a', '#ffa53b','#BB8FCE']
-pivot_table.plot.bar(stacked=True, color=colors, ax=ax)
-
-plt.legend(title='Gender', bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.xlabel('Season')
-plt.ylabel('Count')
-plt.title('Gender Distribution by Season')
-
+pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
+plt.axis('equal')
 st.pyplot(fig)
+
+#fig, ax = plt.subplots(figsize=(15, 7))
+#colors = ['#F7DC6F', '#7fc15a', '#ffa53b','#BB8FCE']
+#pivot_table.plot.bar(stacked=True, color=colors, ax=ax)
+
+#plt.legend(title='Gender', bbox_to_anchor=(1.05, 1), loc='upper left')
+#plt.xlabel('Season')
+#plt.ylabel('Count')
+#plt.title('Gender Distribution by Season')
+
+#st.pyplot(fig)
 
 #grouped_data = df.groupby(['Season', 'Gender']).size().reset_index(name='count')
 #pivot_table = pd.pivot_table(grouped_data, values='count', index='Season', columns='Gender', fill_value=0)
