41fa859aacecb7757abf9ed8ffa161510f458215
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-19 21:27:32 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -8,3 +8,14 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:",layout="wide")
 
 st.title(":bar_chart: Simple SuperStore EDA")
+st.title(" :bar_chart: Sample SuperStore EDA")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+
+fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+if fl is not None:
+    filename = fl.name
+    st.write(filename)
+    df = pd.read_csv(filename, encoding = "ISO-8859-1")
+else:
+    os.chdir(r"C:\Users\AEPAC\Desktop\Streamlit")
+    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
