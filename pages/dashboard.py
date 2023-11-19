cd350be4a0f8340823093b728e0f6c140761a73b
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-19 21:31:44 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -7,8 +7,13 @@ warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:",layout="wide")
 
-st.title(":bar_chart: Simple SuperStore EDA")
 st.title(" :bar_chart: Sample SuperStore EDA")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+
+fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+if fl is not None:
+    filename = fl.name
+    st.write st.title(" :bar_chart: Sample SuperStore EDA")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
 fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
@@ -19,6 +24,7 @@ if fl is not None:
 else:
     os.chdir(r"C:\Users\AEPAC\Desktop\Streamlit")
     df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+
 col1, col2 = st.columns((2))
 df["Order Date"] = pd.to_datetime(df["Order Date"])
 
@@ -26,6 +32,21 @@ df["Order Date"] = pd.to_datetime(df["Order Date"])
 startDate = pd.to_datetime(df["Order Date"]).min()
 endDate = pd.to_datetime(df["Order Date"]).max()
 
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+st.sidebar.header("Choose your filter: ")
+# Create for Region
+region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
 with col1:
     date1 = pd.to_datetime(st.date_input("Start Date", startDate))
 
