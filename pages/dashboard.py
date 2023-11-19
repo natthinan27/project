a9a3630ed0f6bfcfe985824f75cb00b4c7ff9c12
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 01:12:53 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -19,8 +19,8 @@ else:
     #os.chdir("C:\\Users\\acer\\Desktop\\shopping")
     #df = pd.read_csv("shopping22.csv", encoding = "ISO-8859-1")
     file_path = r"C:\Users\acer\Desktop\shopping\your_file.txt"         
-    #file_path = "your_file.txt"
-    #file_path = "subfolder/your_file.txt"
+    file_path = "your_file.txt"
+    file_path = "subfolder/your_file.txt"
 #file_path = "/absolute/path/to/shopping22.csv"
 #file_path = "shopping22.csv"
 df = pd.read_csv(file_path,encoding="ISO-8859-1")
