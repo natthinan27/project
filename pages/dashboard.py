4d2490d6c2344ce08830b93382083a41d453a4b8
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 00:52:02 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -14,8 +14,8 @@ fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","x
 if fl is not None:
     filename = fl.name
     st.write(filename)
-    df = pd.read_csv(filename,encoding ="ISO-8859-1")
+    df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
     os.chdir(r"C:\Users\acer\Desktop\shopping")
-    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
+    df = pd.read_csv("shopping22.csv", encoding = "ISO-8859-1")
                                                                     
