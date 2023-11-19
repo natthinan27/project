4a22ad2838c9d2932d50dceafc1d41013e3c7775
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-19 21:22:50 +0700

    .

---
+++ pages/dashboard.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:",layout="wide")
+
+st.title(":bar_chart: Simple SuperStore EDA")
