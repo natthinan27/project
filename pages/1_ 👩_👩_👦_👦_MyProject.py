e388f8f028a3de3e7e39e6bc432f8ffef9673129
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 23:36:52 +0700

    .

--- pages/1_ 👩_👩_👦_👦_MyProject.py
+++ pages/1_ 👩_👩_👦_👦_MyProject.py
@@ -9,6 +9,16 @@ st.set_page_config(
 )
 st.sidebar.success("เลือกรายการด้านบน.")
 
+html_1 = """
+<div style="background-color:#AEC3AE;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การวิเคราะห์พฤติกรรมผู้บริโภคโดยการจัดกลุ่มแบบเคมีน</h5></center>
+<center><h5>Analyzes Consumer Behavior by K-means clustering</h5></center>
+</div>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
 st.write("# การวิเคราะห์พฤติกรรมผู้บริโภคโดยการจัดกลุ่มแบบเคมีน ")
 st.write("# Analyzes Consumer Behavior by K-means clustering")
 st.write("### 2.วัตถุประสงค์")
