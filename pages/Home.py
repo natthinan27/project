e9495b58cc2dca96206b29a23009a1e4cda0c1ee
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-21 00:00:02 +0700

    z

--- pages/Home.py
+++ pages/Home.py
@@ -11,7 +11,7 @@ st.sidebar.success("เลือกรายการด้านบน.")
 
 html_1 = """
 <div style="background-color:#dab3ff;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>การวิเคราะห์พฤติกรรมผู้บริโภคโดยการจัดกลุ่มแบบเคมีน</h5></center>
+<center><h5>การวิเคราะห์พฤติกรรมผู้บริโภคโดยการจัดกลุ่มแบบเคมีน111</h5></center>
 <center><h5>Analyzes Consumer Behavior by K-means clustering</h5></center>
 </div>
 </div>
