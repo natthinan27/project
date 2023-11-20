ad4cc7f820f241b5c210f3b233faa288832ca714
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 19:56:56 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -10,12 +10,6 @@ st.set_page_config(page_title="Shoppinge!!!", page_icon=":bar_chart:",layout="wi
 st.title(" :bar_chart: Shopping")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
-fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
-if fl is not None:
-    filename = fl.name
-    st.write(filename)
-    df = pd.read_csv(filename, encoding = "ISO-8859-1")
-
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
