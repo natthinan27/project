ae483a144602a8d847330000fed179affc02cac9
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 22:03:34 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -13,8 +13,8 @@ st.write(df.head(10))
 st.header("Show chart")
 chart_data=pd.read_csv("./Data/shopping22.csv")
 
-grouped_data = df.groupby(['size', 'Gender']).size().reset_index(name='count')
-pivot_table = pd.pivot_table(grouped_data, values='count', index='size', columns='Gender', fill_value=0)
+grouped_data = df.groupby(['Gender', 'size']).size().reset_index(name='count')
+pivot_table = pd.pivot_table(grouped_data, values='count', index='Gender', columns='size', fill_value=0)
 fig, ax = plt.subplots(figsize=(15, 7))
 colors = ['#57b199', '#7fc15a', '#ffa53b']
 pivot_table.plot.pie(subplots=True, autopct='%1.1f%%', ax=ax, colors=colors)
