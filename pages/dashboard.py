8cad486bdf675460ebdc5b9ca2d4569847bee715
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 19:30:19 +0700

    .

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -16,11 +16,24 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    #os.chdir("C:\\Users\\acer\\Desktop\\shopping")
-    #df = pd.read_csv("shopping22.csv", encoding = "ISO-8859-1")
-    file_path = "/absolute/path/to/shopping22.csv"
-    file_path = "shopping22.csv"
-    df = pd.read_csv(file_path, encoding="latin1")
-#file_path = "/absolute/path/to/shopping22.csv"
-#file_path = "shopping22.csv"
-    df = pd.read_csv(file_path, encoding="utf-8")
+    os.chdir("C:\\Users\\acer\\Desktop\\shopping")
+    df = pd.read_csv("shopping22.csv", encoding = "ISO-8859-1")
+    
+    col1.col2 = st.colums((2))
+    df["Order Data"] = pd.to_datetime(df["Order Data"])
+
+    # Getting the min and max data
+    startData = pd.to_datatime(df["Order Data"])
+    endData = pd.to_datatime(df["Order Data"])
+
+    with col1:
+        date1 = pd.to_datatime(st.date_input("start Data",startData))
+
+    with col2:
+        date2 = pd.to_datatime(st.date_input("End Data",endData))
+
+    df = df[(df["Order Data"] >= date1) & (df["Order Data"] <= date2)].copy()
+
+    st.sidebar.header("Choose your filter:")
+    region = st.sidebar.multiselect("Pick  your Region",df["Region"].unique())
+
