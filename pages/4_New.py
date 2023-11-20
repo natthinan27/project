460319eee82eafaf3997d9e17206cd7c87fa8346
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-20 22:06:28 +0700

    .

--- pages/4_New.py
+++ pages/4_New.py
@@ -13,8 +13,6 @@ st.write(df.head(10))
 st.header("Show chart")
 chart_data=pd.read_csv("./Data/shopping22.csv")
 
-
-
 #st.altair_chart(altair_chart, use_container_width=False, theme="streamlit")
 group_age = df.groupby(['Item Purchased', 'Gender']).size().reset_index(name='count')
 st.bar_chart(group_age, x='Item Purchased', y='count', color='Gender', height=400)
